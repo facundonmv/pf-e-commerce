2e8691fe30171eb694f043d244182752b002974c
facundonmv <facundonmorata@gmail.com>
2022-11-25 14:26:06 -0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+
+function App() {
+  return (
+    
+    <>
+    
+      <Switch>
+ 
+      </Switch>
+    </>
+  );
+}
+
+export default App;
