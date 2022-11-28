4f6b977a747c7f1b1cd04e70784d011b96c27195
Daniel Gonzalez <daniel99gs.0@gmail.com>
2022-11-27 21:34:26 -0300

    Form de producto y navbar

---
+++ client/src/Redux/reducer/reduce.js
@@ -0,0 +1,21 @@
+
+const initialState={
+    product:[],
+    category:[]
+
+}
+
+
+function rootReducer(state = initialState, action){
+    switch (action.type) {
+        case "GET_PRODUCT" :
+            
+            break;
+    
+        default:
+            break;
+    }
+
+}
+
+export default rootReducer;
